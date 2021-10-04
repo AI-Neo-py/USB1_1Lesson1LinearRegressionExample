bca0cf13baa9e3e932f0ded9c4287b5543a2cc02
school-ehayashi1 <91492985+school-ehayashi1@users.noreply.github.com>
2021-10-04 10:44:08 -0700

    Delete LinearRegressionExampleUSB1_1Lesson1_2.py

--- LinearRegressionExampleUSB1_1Lesson1_2.py
+++
@@ -1,9 +0,0 @@
-# Sklearn module
-# Rough outline of regular sklearn code in regular coding.
-from sklearn.linear_model import Linear Regression
-import pandas as pd
-X, y = pd.read_csv(“house_csv”)
-#preprocessing...
-Linreg = LinearRegression()
-Linreg.fit(X, y)
-Linreg.predict(Xtr)
